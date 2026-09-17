fe4e5f3f0502a50698357db13ce27ac4d683973d
Kishor Devkota <mekishor61@gmail.com>
2026-09-17 10:44:29 +0100

    add registration page and tests

---
+++ NopCommerce.UI.Tests/Pages/RegisterPage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace NopCommerce.UI.Tests.Pages
+{
+    public sealed class RegisterPage : BasePage
+    {
+        public RegisterPage(IPage page) : base(page)
+        {
+        }
+
+        private ILocator Firstname =>
+            Page.Locator("#FirstName");
+        private ILocator Lastname =>
+            Page.Locator("#LastName");
+
+        private ILocator Email =>
+            Page.Locator("#Email");
+
+        private ILocator Company =>
+            Page.Locator("#Company");
+
+        private ILocator Password =>
+            Page.Locator("#Password");
+
+        private ILocator ConfirmPassword =>
+            Page.Locator("#ConfirmPassword");
+
+
+
+        private ILocator RegisterButton =>
+            Page.Locator("button.register-next-step-button");
+
+        private ILocator TextMessage =>
+           Page.Locator(".result");
+
+        public async Task OpenAsync()
+        {
+            await NavigateAsync("register");
+        }
+
+        public async Task RegisterAsync(
+            string firstname, string lastname, string email, string company,
+             string password, string confirmpassword)
+        {
+            await Firstname.FillAsync(firstname);
+            await Lastname.FillAsync(lastname);
+            await Email.FillAsync(email);
+            await Company.FillAsync(company);
+            await Password.FillAsync(password);
+            await ConfirmPassword.FillAsync(confirmpassword);
+
+            await RegisterButton.ClickAsync();
+
+            await Page.WaitForLoadStateAsync(
+                LoadState.NetworkIdle);
+        }
+
+        public async Task<string> RegisterMessageAsync()
+        {
+            return await TextMessage.InnerTextAsync();
+        }
+
+
+
+    }
+}
---
+++ NopCommerce.UI.Tests/Tests/RegistrationTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using NopCommerce.TestFramework.Browser;
+using NopCommerce.TestFramework.Configuration;
+using NopCommerce.UI.Tests.Pages;
+
+
+namespace NopCommerce.UI.Tests.Tests
+{
+    public sealed class RegistrationTests : BaseTest
+    {
+        [Test]
+        [Category("Smoke")]
+        public async Task CustomerCanRegister()
+        {
+            var settings = ConfigurationManager.Settings;
+
+            var registerPage = new RegisterPage(Page);
+
+            
+
+            await registerPage.OpenAsync();
+
+            await registerPage.RegisterAsync(
+                settings.TestRegisterUser.Firstname,
+                settings.TestRegisterUser.Lastname,
+                settings.TestRegisterUser.Email,
+                settings.TestRegisterUser.Company,
+                settings.TestRegisterUser.Password,
+                settings.TestRegisterUser.Password);
+                var message = await registerPage.RegisterMessageAsync();
+                message.Should().Contain("Your registration completed");
+        }
+
+    }
+}
